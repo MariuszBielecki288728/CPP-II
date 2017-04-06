bf34733df870da4dfe046940598133b6302c72a7
Mariusz <288728@uwr.edu.pl>
2017-04-07 00:15:42 +0200

    MarcZad

--- Lista 4/BuforyJakiesEh/BuforyJakiesEh/header.h
+++ Lista 4/BuforyJakiesEh/BuforyJakiesEh/header.h
@@ -1,26 +1,114 @@
 #pragma once
-using namespace std;
-int tab[1000];
+#include "stdafx.h"
+#include <iostream>
+#include <algorithm>
 
-class Lista {
-	protected:
-	class Node {
-		Node();
-		Node* prev;
-		double val;
-		Node* next;
-		void wstaw(double, int);
-		void usun(int);
-		int rozmiar();
-		Node& operator[](int);
-		void* operator new(size_t);
-		void operator delete(void* wsk);
 
+class lista
+{
+protected:
+	class wezel
+	{
+	public:
+
+		wezel *previous;
+		double value;
+		wezel *next;
+
+		wezel() = default;
+		wezel(double val);
+		wezel(const wezel &pattern);
+		wezel(wezel &&pattern);
+		wezel* wstaw(int pos, double val);
+		double usun(int pos);
+		wezel& index(int pos);
+		wezel& operator=(const wezel &pattern);
+		wezel& operator=(wezel &&pattern);
+		static void* operator new(size_t);
+		static void operator delete(void *address);
+
+		friend std::ostream& operator<<(std::ostream& wy, const wezel& pattern)
+		{
+			return wy << pattern.value;
+		}
 	};
+	wezel *first;
+	wezel *last;
+	int size;
 public:
-	Node* first;
-	Node* last;
-};
-class kolejka : Lista {
+	lista();
+	lista(const lista &pattern);
+	lista(lista &&pattern);
+	~lista();
+	lista& operator=(const lista &pattern);
+	lista& operator=(lista &&pattern);
+	void wstaw(int pos, double val);
+	double usun(int pos);
+	int getSize() const;
+	wezel& operator[](int pos) const;
 
+	friend std::istream& operator>>(std::istream &we, lista &list);
+	friend std::ostream& operator<<(std::ostream &wy, const lista &pattern);
+	friend std::ostream& operator<<(std::ostream& wy, const wezel& pattern);
+	friend class Memory;
+
+	
+};
+
+
+class kolejka : protected lista
+{
+	//posiadam chronione pola first, last oraz size. mam tez odziedziczona klase wezel
+public:
+	using lista::lista; //dziedziczenie konstruktorow
+	kolejka& operator+=(double val);
+	double operator--();
+	double operator*();
+	void wstaw(double val);
+	double usun();
+	double gotowy();
+	int ile();
+};
+
+
+class stos : protected lista
+{
+	//posiadam chronione pola first, last oraz size. mam tez odziedziczona klase wezel
+public:
+	using lista::lista;
+	stos& operator+=(double val);
+	double operator--();
+	double operator*();
+	void wstaw(double val);
+	double usun();
+	double gotowy();
+	int ile();
+};
+
+
+
+class Memory
+{
+private:
+	static const int size = 10;
+	static lista::wezel ground[size];
+	static bool flags[size]; //1 - free, 0 - occupied
+	Memory()
+	{
+		std::fill_n(flags, size, 1);
+	}
+
+public:
+	static Memory& getInstance()
+	{
+		static Memory instance; //statyczny obiekt jest inicjalizowany przy pierwszym wywolaniu i istnieje do konca programu
+		return instance;
+	}
+	~Memory() = default;
+	Memory(Memory const&) = delete;
+	Memory(Memory &&) = delete;
+	void operator=(Memory const&) = delete;
+	void operator=(Memory &&) = delete;
+	lista::wezel* getAddress();
+	void freeAddress(lista::wezel *address);
 };
